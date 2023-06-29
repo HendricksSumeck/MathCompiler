eff437c05037413aef8992862cb3ded2e60009e4
hsume <hsumeck@gmail.com>
2023-06-28 21:24:43 -0300

    Compile code in main

--- main.c
+++ main.c
@@ -1,9 +1,35 @@
+//
+// Created by hsume on 27/06/2023.
+//
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "queue.h"
 #include "execute.h"
 #include "compiler.h"
 
+void Compile()
+{
+    // Exemplo de código-fonte em Minilang
+    char* sourceCode = "2 + 3 * 4";
+
+    // Compilar o código-fonte
+    char* compiledCode = compiler(sourceCode);
+
+    // Imprimir o código compilado
+    printf("Codigo-fonte original: %s\n", sourceCode);
+    printf("Codigo compilado:\n%s\n", compiledCode);
+
+    // Executar o código compilado até o ponto de exibir o resultado
+    int result = executeCode(compiledCode);
+    printf("Resultado: %d\n", result);
+
+    // Liberar memória alocada
+    free(compiledCode);
+}
+
 int main() {
-    printf("Hello, World!\n");
+    Compile();
     return 0;
 }
